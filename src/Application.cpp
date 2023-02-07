a7ca4028c853ed07b88d212f0ec3418ff403c698
HudsonSchumaker <hudson.schumaker@me.com>
2023-02-07 22:10:01 +0100

    App

--- src/Application.cpp
+++ src/Application.cpp
@@ -14,10 +14,26 @@ bool Application::IsRunning() {
 void Application::Setup() {
     running = Graphics::OpenWindow();
 
-    Body* bigBall = new Body(CircleShape(100), 100, 100, 1.0);
-    Body* smallBall = new Body(CircleShape(50), 500, 100, 1.0);
-    bodies.push_back(bigBall);
-    bodies.push_back(smallBall);
+    // Create a physics world with gravity of -9.8 m/s2
+    world = new World(-9.8);
+
+    // Add a floor and walls to contain objects
+    Body* floor = new Body(BoxShape(Graphics::Width() - 50, 50), Graphics::Width() / 2.0, Graphics::Height() - 50, 0.0);
+    Body* leftWall = new Body(BoxShape(50, Graphics::Height() - 100), 50, Graphics::Height() / 2.0 - 25, 0.0);
+    Body* rightWall = new Body(BoxShape(50, Graphics::Height() - 100), Graphics::Width() - 50, Graphics::Height() / 2.0 - 25, 0.0);
+    floor->restitution = 0.5;
+    leftWall->restitution = 0.2;
+    rightWall->restitution = 0.2;
+    world->AddBody(floor);
+    world->AddBody(leftWall);
+    world->AddBody(rightWall);
+
+    // Add a static box so other objects can collide
+    Body* bigBox = new Body(BoxShape(200, 200), Graphics::Width() / 2.0, Graphics::Height() / 2.0, 0.0);
+    bigBox->SetTexture("./assets/crate.png");
+    bigBox->restitution = 0.7;
+    bigBox->rotation = 1.4;
+    world->AddBody(bigBox);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -33,13 +49,26 @@ void Application::Input() {
             case SDL_KEYDOWN:
                 if (event.key.keysym.sym == SDLK_ESCAPE)
                     running = false;
+                if (event.key.keysym.sym == SDLK_d)
+                    debug = !debug;
                 break;
-            case SDL_MOUSEMOTION:
-                int x, y;
-                SDL_GetMouseState(&x, &y);
-                bodies[0]->position.x = x;
-                bodies[0]->position.y = y;
-                // ...
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    int x, y;
+                    SDL_GetMouseState(&x, &y);
+                    Body* ball = new Body(CircleShape(30), x, y, 1.0);
+                    ball->SetTexture("./assets/basketball.png");
+                    ball->restitution = 0.7;
+                    world->AddBody(ball);
+                }
+                if (event.button.button == SDL_BUTTON_RIGHT) {
+                    int x, y;
+                    SDL_GetMouseState(&x, &y);
+                    Body* box = new Body(BoxShape(60, 60), x, y, 1.0);
+                    box->SetTexture("./assets/crate.png");
+                    box->restitution = 0.2;
+                    world->AddBody(box);
+                }
                 break;
         }
     }
@@ -65,60 +94,8 @@ void Application::Update() {
     // Set the time of the current frame to be used in the next one
     timePreviousFrame = SDL_GetTicks();
 
-    // Apply forces to the bodies
-    for (auto body: bodies) {
-        // Apply the weight force
-        // Vec2 weight = Vec2(0.0, body->mass * 9.8 * PIXELS_PER_METER);
-        // body->AddForce(weight);
-
-        // Apply the wind force
-        // Vec2 wind = Vec2(20.0 * PIXELS_PER_METER, 0.0);
-        // body->AddForce(wind);
-    }
-
-    // Integrate the acceleration and velocity to estimate the new position
-    for (auto body: bodies) {
-        body->Update(deltaTime);
-    }
-
-    // Check all the rigidbodies with the other rigidbodies for collision
-    for (int i = 0; i <= bodies.size() - 1; i++) {
-        for (int j = i + 1; j < bodies.size(); j++) {
-            Body* a = bodies[i];
-            Body* b = bodies[j];
-            a->isColliding = false;
-            b->isColliding = false;
-            Contact contact;
-            if (CollisionDetection::IsColliding(a, b, contact)) {
-                Graphics::DrawFillCircle(contact.start.x, contact.start.y, 3, 0xFFFF00FF);
-                Graphics::DrawFillCircle(contact.end.x, contact.end.y, 3, 0xFFFF00FF);
-                Graphics::DrawLine(contact.start.x, contact.start.y, contact.start.x + contact.normal.x * contact.depth , contact.start.y + contact.normal.y * contact.depth, 0xFFFF00FF);
-                a->isColliding = true;
-                b->isColliding = true;
-            }
-        }
-    }
-
-    // Check the boundaries of the window applying a hardcoded bounce flip in velocity
-    for (auto body: bodies) {
-        if (body->shape->GetType() == CIRCLE) {
-            CircleShape* circleShape = (CircleShape*) body->shape;
-            if (body->position.x - circleShape->radius <= 0) {
-                body->position.x = circleShape->radius;
-                body->velocity.x *= -0.9;
-            } else if (body->position.x + circleShape->radius >= Graphics::Width()) {
-                body->position.x = Graphics::Width() - circleShape->radius;
-                body->velocity.x *= -0.9;
-            }
-            if (body->position.y - circleShape->radius <= 0) {
-                body->position.y = circleShape->radius;
-                body->velocity.y *= -0.9;
-            } else if (body->position.y + circleShape->radius >= Graphics::Height()) {
-                body->position.y = Graphics::Height() - circleShape->radius;
-                body->velocity.y *= -0.9;
-            }
-        }
-    }
+    // Update world bodies (integration, collision detection, etc.)
+    world->Update(deltaTime);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -126,19 +103,32 @@ void Application::Update() {
 ///////////////////////////////////////////////////////////////////////////////
 void Application::Render() {
     // Draw all bodies
-    for (auto body: bodies) {
-        Uint32 color = body->isColliding ? 0xFF0000FF : 0xFFFFFFFF;
-
+    for (auto body: world->GetBodies()) {
         if (body->shape->GetType() == CIRCLE) {
             CircleShape* circleShape = (CircleShape*) body->shape;
-            Graphics::DrawCircle(body->position.x, body->position.y, circleShape->radius, body->rotation, color);
+            if (!debug && body->texture) {
+                Graphics::DrawTexture(body->position.x, body->position.y, circleShape->radius * 2, circleShape->radius * 2, body->rotation, body->texture);
+            } else {
+                Graphics::DrawCircle(body->position.x, body->position.y, circleShape->radius, body->rotation, 0xFF00FF00);
+            }
         }
         if (body->shape->GetType() == BOX) {
             BoxShape* boxShape = (BoxShape*) body->shape;
-            Graphics::DrawPolygon(body->position.x, body->position.y, boxShape->worldVertices, 0xFFFFFFFF);
+            if (!debug && body->texture) {
+                Graphics::DrawTexture(body->position.x, body->position.y, boxShape->width, boxShape->height, body->rotation, body->texture);
+            } else {
+                Graphics::DrawPolygon(body->position.x, body->position.y, boxShape->worldVertices, 0xFF00FF00);
+            }
+        }
+        if (body->shape->GetType() == POLYGON) {
+            PolygonShape* polygonShape = (PolygonShape*) body->shape;
+            if (!debug) {
+                Graphics::DrawFillPolygon(body->position.x, body->position.y, polygonShape->worldVertices, 0xFF444444);
+            } else {
+                Graphics::DrawPolygon(body->position.x, body->position.y, polygonShape->worldVertices, 0xFF00FF00);
+            }
         }
     }
-
     Graphics::RenderFrame();
 }
 
@@ -146,8 +136,6 @@ void Application::Render() {
 // Destroy function to delete objects and close the window
 ///////////////////////////////////////////////////////////////////////////////
 void Application::Destroy() {
-    for (auto body: bodies) {
-        delete body;
-    }
+    delete world;
     Graphics::CloseWindow();
 }
