e8146fa23b82b227ea0f4f9a1c2a05c9478637b8
HudsonSchumaker <hudson.schumaker@me.com>
2023-02-07 22:08:31 +0100

    Body

--- src/Physics/Body.h
+++ src/Physics/Body.h
@@ -1,6 +1,8 @@
 #ifndef BODY_H
 #define BODY_H
 
+#include <SDL.h>
+#include <SDL_image.h>
 #include "Vec2.h"
 #include "Shape.h"
 
@@ -27,16 +29,32 @@ struct Body {
     float I;
     float invI;
 
+    // Coefficient of restitution (elasticity)
+    float restitution;
+
+    // Coefficient of friction
+    float friction;
+
     // Pointer to the shape/geometry of this rigid body
     Shape* shape = nullptr;
 
+    // Pointer to an SDL texture
+    SDL_Texture* texture = nullptr;
+
     Body(const Shape& shape, float x, float y, float mass);
     ~Body();
+
+    bool IsStatic() const;
 
     void AddForce(const Vec2& force);
     void AddTorque(float torque);
     void ClearForces();
     void ClearTorque();
+
+    void SetTexture(const char* textureFileName);
+
+    void ApplyImpulse(const Vec2& j);
+    void ApplyImpulse(const Vec2& j, const Vec2& r);
 
     void IntegrateLinear(float dt);
     void IntegrateAngular(float dt);
